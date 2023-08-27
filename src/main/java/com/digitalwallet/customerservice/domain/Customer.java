c9c2e219d53fe5a734055bedb47913edf6407dab
Orlando Avila <orlando.avilag@hotmail.com>
2023-08-27 16:14:24 -0600

    Phone number value object created and db configuration updated.

--- src/main/java/com/digitalwallet/customerservice/domain/Customer.java
+++ src/main/java/com/digitalwallet/customerservice/domain/Customer.java
@@ -2,4 +2,14 @@ package com.digitalwallet.customerservice.domain;
 
 public class Customer {
     private CustomerDocumentNumber documentNumber;
+    private CustomerPhoneNumber phoneNumber;
+    private CustomerTypeDocument typeDocument;
+    private  CustomerEmail email;
+
+    public Customer(String documentNumber, String phoneNumber, String typeDocument, String email) {
+        this.documentNumber = new CustomerDocumentNumber(documentNumber);
+        this.phoneNumber = new CustomerPhoneNumber(phoneNumber);
+        this.typeDocument = new CustomerTypeDocument(typeDocument);
+        this.email = new CustomerEmail(email);
+    }
 }
